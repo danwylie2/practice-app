1f0659ba885c67ee71f896cd217d0920879d0755
dan <200030341@student.kangan.edu.au>
2023-11-13 15:40:57 +1100

    bastard

---
+++ src/routes/SelectedUser.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import { Heading, Input, Label } from 'flowbite-svelte';
+    export let selectedUser: User;
+    console.log("selectedUser", selectedUser);
+    console.log("selectedUser ID", selectedUser.id);
+
+</script>
+
+
+<Heading>Selected User:</Heading>
+    <br>
+    <form>
+        <div class="grid gap-6 mb-6 md:grid-cols-2 ">
+            <div>
+                <Label for="id">ID</Label>
+                <Input bind:value={selectedUser.id} type="number" id="id" />
+            </div>
+            <div>
+                <Label for="name">Name</Label>
+                <Input bind:value={selectedUser.name} type="text" id="name" />
+            </div>
+            <div>
+                <Label for="username">Username</Label>
+                <Input bind:value={selectedUser.username} type="text" id="username" />
+            </div>
+            <div>
+                <Label for="phone">Phone</Label>
+                <Input bind:value={selectedUser.phone} type="text" id="phone" />
+            </div>
+            <div>
+                <Label for="website">Website</Label>
+                <Input bind:value={selectedUser.website} type="text" id="website" />
+            </div>
+        </div>
+    </form>
--- src/routes/UsersList.svelte
+++ src/routes/UsersList.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
     import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox, TableSearch } from 'flowbite-svelte';
+    
+    import { createEventDispatcher } from 'svelte'; 
+
+    const dispatch = createEventDispatcher();
 
     export let users: User[];
 
     function rowClickHandler(event: any) {
         console.log("row clicked", event.target.parentNode.parentNode.id);
+        dispatch('select', { id: event.target.parentNode.parentNode.id });
     }
 </script>
 
@@ -25,20 +30,22 @@
             </TableHeadCell>
         </TableHead>
         <TableBody>
-            <TableBodyRow id="0" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <TableHeadCell scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    0
-                </TableHeadCell>
-                <TableBodyCell class="px-6 py-4">
-                    SampleName
-                </TableBodyCell>
-                <TableBodyCell class="px-6 py-4">
-                    SampleUsername
-                </TableBodyCell>
-                <TableBodyCell class="px-6 py-4">
-                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" on:click={rowClickHandler}>Select</button>
-                </TableBodyCell>
-            </TableBodyRow>
+            {#each users as user}
+                <TableBodyRow id={user.id} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <TableHeadCell scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {user.id}
+                    </TableHeadCell>
+                    <TableBodyCell class="px-6 py-4">
+                        {user.name}
+                    </TableBodyCell>
+                    <TableBodyCell class="px-6 py-4">
+                        {user.username}
+                    </TableBodyCell>
+                    <TableBodyCell class="px-6 py-4">
+                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" on:click={rowClickHandler}>Select</button>
+                    </TableBodyCell>
+                </TableBodyRow>
+            {/each}
         </TableBody>
     </Table>
 </div>
